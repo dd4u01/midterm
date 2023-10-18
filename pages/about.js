5331730f07088e28218cd04bd2596a33358222cf
dd4u01 <dd4u01@gmail.com>
2023-10-18 13:23:48 +0900

    midterm 2nd commit

---
+++ pages/about.js
@@ -0,0 +1,3 @@
+export default function about(){
+    return <p>C:\2023\React2\midterm\pages\about.js</p>
+  }
---
+++ pages/contact.js
@@ -0,0 +1,3 @@
+export default function contact(){
+    return <p>C:\2023\React2\midterm\pages\contact.js</p>
+  }
